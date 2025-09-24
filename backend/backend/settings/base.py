fa07188626f33772eb137351d4f5b92b0430bf90
mirshod <oripovmirshod9@gmail.com>
2025-09-24 21:55:58 +0900

    startup.sh file created

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -268,7 +268,7 @@ load_dotenv(BASE_DIR / ".env")
 
 
 EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT", ""))
+EMAIL_PORT = (os.getenv("EMAIL_PORT", ""))
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
